54bec05d26578d94d1d36d1ce644cf57709e94bb
Nomathwords <70309839+Nomathwords@users.noreply.github.com>
2022-11-14 20:11:46 -0600

    Add files via upload

--- Individual Project Tables.sql
+++ Individual Project Tables.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS Product1
+/*DROP TABLE IF EXISTS Product1
 DROP TABLE IF EXISTS Product2
 DROP TABLE IF EXISTS Product3
 DROP TABLE IF EXISTS Technical_Staff_Degree
@@ -36,6 +36,7 @@ CREATE TABLE Technical_Staff_Degree (
    Name varchar(20),
    Degree varchar(5),
 
+   CONSTRAINT chk_Degree_Type CHECK(Degree = 'BS' OR Degree = 'MS' OR Degree = 'PHD'),
    PRIMARY KEY (Name, Degree),
    FOREIGN KEY (Name) REFERENCES Technical_Staff(Technical_Staff_Name)
 )
@@ -109,8 +110,9 @@ CREATE TABLE Complaint (
 
 CREATE TABLE Purchase (
    Customer_Name varchar(20),
-   Product_ID INT PRIMARY KEY,
+   Product_ID INT,
 
+   PRIMARY KEY (Customer_Name, Product_ID),
    FOREIGN KEY (Customer_Name) REFERENCES Customer(Name),
    FOREIGN KEY (Product_ID) REFERENCES Product(ID)
 );
@@ -169,6 +171,7 @@ CREATE TABLE Repair_Complaint (
 
 ----------------------------------------------------------------------------------------
 
+-- Insert employees
 INSERT INTO Employee(Employee_Name, Address, Salary, Employee_Type)
 VALUES('Hunter', '2325 Louise Ln', 10000, 'Technical Staff')
 
@@ -176,13 +179,31 @@ INSERT INTO Technical_Staff(Technical_Staff_Name, Position)
 VALUES('Hunter', 'IT')
 
 INSERT INTO Technical_Staff_Degree(Name, Degree)
-VALUES ('Hunter', 'IT')
+VALUES ('Hunter', 'BS')
 
 INSERT INTO Employee(Employee_Name, Address, Salary, Employee_Type)
-VALUES('Jackson', '1410 Wisteria Ave', 10000, 'Worker')
+VALUES('Steven', '1321 E 24th St', 50000, 'Technical Staff')
+
+INSERT INTO Technical_Staff(Technical_Staff_Name, Position)
+VALUES('Steven', 'Manager')
+
+INSERT INTO Technical_Staff_Degree(Name, Degree)
+VALUES ('Steven', 'BS')
+
+INSERT INTO Technical_Staff_Degree(Name, Degree)
+VALUES ('Steven', 'MS')
+
+INSERT INTO Employee(Employee_Name, Address, Salary, Employee_Type)
+VALUES('Jackson', '1410 Wisteria Ave', 32000, 'Worker')
 
 INSERT INTO Worker(Worker_Name, Max_Products)
 VALUES('Jackson', 23)
+
+INSERT INTO Employee(Employee_Name, Address, Salary, Employee_Type)
+VALUES('Dieko', '1309 Traditions Way', 28000, 'Worker')
+
+INSERT INTO Worker(Worker_Name, Max_Products)
+VALUES('Dieko', 20)
 
 INSERT INTO Employee(Employee_Name, Address, Salary, Employee_Type)
 VALUES('Ansley', '1410 Wisteria Ave', 10000, 'Quality Controller')
@@ -190,11 +211,30 @@ VALUES('Ansley', '1410 Wisteria Ave', 10000, 'Quality Controller')
 INSERT INTO Quality_Controller(Quality_Controller_Name, Product_Type)
 VALUES('Ansley', 'Toys')
 
+INSERT INTO Employee(Employee_Name, Address, Salary, Employee_Type)
+VALUES('Ronald', '1100 S Ocean Blvd, Palm Beach', 100000, 'Quality Controller')
+
+INSERT INTO Quality_Controller(Quality_Controller_Name, Product_Type)
+VALUES('Ronald', 'Flags')
+
+-- Insert Products
 INSERT INTO Product(ID, Date_Created, Days_Developed, Produced_By, Tested_By, Repaired_By, Size, Product_Type)
 VALUES(1, '11/13/2022', 3, 'Jackson', 'Ansley', null, 'Large', 1)
 
 INSERT INTO Product1(Product1_ID, Software)
 VALUES(1, 'Java')
+
+INSERT INTO Product(ID, Date_Created, Days_Developed, Produced_By, Tested_By, Repaired_By, Size, Product_Type)
+VALUES(4, '10/25/2019', 65, 'Dieko', 'Ronald', null, 'Extra Small', 1)
+
+INSERT INTO Product1(Product1_ID, Software)
+VALUES(4, 'C++')
+
+INSERT INTO Product(ID, Date_Created, Days_Developed, Produced_By, Tested_By, Repaired_By, Size, Product_Type)
+VALUES(5, '07/04/2016', 12, 'Dieko', 'Ansley', 'Steven', 'Medium', 1)
+
+INSERT INTO Product1(Product1_ID, Software)
+VALUES(5, 'Ruby')
 
 INSERT INTO Product(ID, Date_Created, Days_Developed, Produced_By, Tested_By, Repaired_By, Size, Product_Type)
 VALUES(2, '11/10/2022', 45, 'Jackson', 'Ansley', 'Hunter', 'Small', 2)
@@ -203,18 +243,50 @@ INSERT INTO Product2(Product2_ID, Color)
 VALUES(2, 'Green')
 
 INSERT INTO Product(ID, Date_Created, Days_Developed, Produced_By, Tested_By, Repaired_By, Size, Product_Type)
-VALUES(3, '11/10/2022', 45, 'Jackson', 'Ansley', 'Hunter', 'Medium', 3)
+VALUES(6, '12/31/1999', 365, 'Dieko', 'Ronald', null, 'Extra Large', 2)
 
 INSERT INTO Product2(Product2_ID, Color)
+VALUES(6, 'Black')
+
+INSERT INTO Product(ID, Date_Created, Days_Developed, Produced_By, Tested_By, Repaired_By, Size, Product_Type)
+VALUES(7, '02/24/2002', 61, 'Jackson', 'Ronald', 'Steven', 'Small', 2)
+
+INSERT INTO Product2(Product2_ID, Color)
+VALUES(7, 'Red')
+
+INSERT INTO Product(ID, Date_Created, Days_Developed, Produced_By, Tested_By, Repaired_By, Size, Product_Type)
+VALUES(3, '11/10/2022', 45, 'Jackson', 'Ansley', 'Hunter', 'Medium', 3)
+
+INSERT INTO Product3(Product3_ID, Weight)
 VALUES(3, 100.75)
 
+INSERT INTO Product(ID, Date_Created, Days_Developed, Produced_By, Tested_By, Repaired_By, Size, Product_Type)
+VALUES(8, '08/02/2005', 02, 'Dieko', 'Ansley', null, 'Small', 3)
 
+INSERT INTO Product3(Product3_ID, Weight)
+VALUES(8, 5.25)
+
+INSERT INTO Product(ID, Date_Created, Days_Developed, Produced_By, Tested_By, Repaired_By, Size, Product_Type)
+VALUES(9, '10/23/2007', 10, 'Dieko', 'Ronald', null, 'Large', 3)
+
+INSERT INTO Product3(Product3_ID, Weight)
+VALUES(9, 107)
+
+-- Insert customers associated with products
 INSERT INTO Customer(Name, Address)
 VALUES('Rebecca', 'Bumfuckville')
 
 INSERT INTO Purchase(Customer_Name, Product_ID)
 VALUES('Rebecca', 1)
 
+INSERT INTO Customer(Name, Address)
+VALUES('Kyle', '1410 Wisteria Ave')
+
+INSERT INTO Purchase(Customer_Name, Product_ID)
+VALUES('Kyle', 1)
+
+
+-- Create product accounts
 INSERT INTO Account(Account_Number, Date_Created)
 VALUES(1, '11/13/2022')
 
@@ -233,6 +305,7 @@ VALUES(1, '11/13/2022', 'Screen is faded', 'Screen replacement', 'Rebecca', 1)
 INSERT INTO Accident(Accident_Number, Date_Created, Work_Days_Lost, Product_ID, Employee_Name, Employee_Type) 
 VALUES(1, '08/25/2000', 45, 2, 'Hunter', 'Technical Staff')
 
+-------------------------------------------------------------------------------------------------------------
 
 SELECT * FROM Employee
 SELECT * FROM Technical_Staff
@@ -250,6 +323,9 @@ SELECT * FROM Product1_Account
 SELECT * FROM Product2_Account
 SELECT * FROM Product3_Account
 SELECT * FROM Complaint
-SELECT * FROM Accident
+SELECT * FROM Accident */
 
-SELECT ID FROM Product WHERE Produced_By = 'Jackson'
+-- Query 9
+SELECT * FROM Product
+SELECT COUNT(ID) FROM Product WHERE Tested_By = 'Ronald' AND Repaired_By IS NOT NULL
+
